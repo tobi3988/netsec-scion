a230360b7a91c43b5dd356fd4fcd86eee924f36e
Tobi <tobias.s.bachmann@gmail.com>
2018-03-22 11:56:08 +0100

    percentile999 and mean

--- python/metric_server/metrics/latency.py
+++ python/metric_server/metrics/latency.py
@@ -4,6 +4,8 @@ from statistics import mean, pvariance
 
 from metric_server.lib.lib import percentile
 
+NORMALDIST999 = 3.09
+
 
 def calculate_one_way_delay_variation(measurements):
     result = defaultdict(lambda: 0)
@@ -49,13 +51,34 @@ def calculate_variance(measurements):
     return pvariance(latencies)
 
 
-def calculate_skew(mean, variance):
-    # TODO implement it.
-    return (6)
-
-
 def calculate_percentile999(measurements):
     if len(measurements) < 2:
         return 0
     latencies = normalize_and_sort_measurements(measurements)
     return percentile(latencies, 0.999)
+
+
+def calculate_third_moment(metric):
+    return calculate_skewness(metric) ** (1.5)
+
+
+def calculate_total_skewness(variance, third_moment):
+    return third_moment / float(variance ** (1.5))
+
+
+def calculate_percentile999_for_path(metrics):
+    mean = sum(metric.mean_normalized for metric in metrics)
+    variance = sum(metric.variance for metric in metrics)
+    third_moment = sum(calculate_third_moment(metric) for metric in metrics)
+    skew = calculate_total_skewness(variance, third_moment)
+    return mean + variance ** 0.5 * (NORMALDIST999 - (skew / 6.0) * (1 - NORMALDIST999 ** 2))
+
+
+def calculate_skewness(metric):
+    percentil = float(metric.percentil999)
+    mean = float(metric.normalized_mean)
+    return 6 * ((NORMALDIST999 - ((percentil - mean) / metric.variance)) / (1 - NORMALDIST999 ** 2))
+
+
+def calculate_one_way_delay_for_path(metrics):
+    return mean(metric.one_way_delay for metric in metrics)
