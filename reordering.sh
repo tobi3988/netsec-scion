a1a61c45c658d150fe8b069ced1008ec9bcffc7a
Tobi <tobias.s.bachmann@gmail.com>
2018-04-07 13:44:58 +0200

    added only reordering

---
+++ reordering.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DURATION=30m
+WARMUPTIME=10m
+CHANGERATE=1s
+
+##########################################################
+
+echo "start experiment constant packet reordering------------------------------"
+tc qdisc del dev lo root netem
+tc qdisc add dev lo root netem delay 20ms reorder 5.556% 50%
+su -c 'cd ${SC} && ./scion.sh stop' - ubuntu
+
+rm -f logs/metrics.csv
+rm -f logs/multipath.csv
+su -c 'cd ${SC} && ./scion.sh start' - ubuntu
+
+sleep $DURATION
+su -c 'cd ${SC} && ./scion.sh stop' - ubuntu
+
+mkdir -p experiments/logs/reordering
+cp logs/metrics.csv experiments/logs/reordering/
+cp logs/multipath.csv experiments/logs/reordering/
+
+##########################################################
+
+tc qdisc del dev lo root netem
